61910083047094810305cd6d4adcb1f12773c961
sonny faristrianto <sonnyf4r15@gmail.com>
2024-01-07 19:48:16 +0700

    add tugas6

---
+++ tugas6.js
@@ -0,0 +1,12 @@
+function perulangan() {
+    for (var i=1; i<=20; i++){
+        var c=i%2
+     
+        if (c==0)
+        console.log("genap")
+        else if(c!=0)
+        console.log(i)
+    }
+}
+
+perulangan();                                        
